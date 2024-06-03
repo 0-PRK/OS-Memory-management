5a26554a70f13118f5fa561dfc72639408667610
0-PRK <116703849+0-PRK@users.noreply.github.com>
2024-06-04 02:55:22 +0545

    Buddy system completed

--- buddy_system.py
+++ buddy_system.py
@@ -1,25 +1,44 @@
 from utils import print_memory_layout, print_allocation_table, exists
+import math
 
 class BuddySystem:
     def __init__(self, total_memory):
         self.total_memory = total_memory
-        self.free_blocks = {total_memory: [0]}  # Dictionary of free blocks by size
-        self.processes = {}  # Dictionary to hold process allocations (process_id: (start, size))
+        self.size = math.ceil(math.log2(total_memory)) + 1
+        self.free_list = [[] for _ in range(self.size)]
+        self.processes = {}
+        self.initialize(total_memory)
+
+    def initialize(self, total_memory):
+        n = math.ceil(math.log2(total_memory))
+        self.free_list[n].append((0, total_memory - 1))
 
     def add_process(self, process_id, memory_required):
-        # size = 1
+        size = 1
         while size < memory_required:
             size *= 2
-        for s in range(size, self.total_memory + 1, size * 2):
-            if s in self.free_blocks and self.free_blocks[s]:
-                start = self.free_blocks[s].pop(0)
-                if not self.free_blocks[s]:
-                    del self.free_blocks[s]
-                if size < s:
-                    self.free_blocks.setdefault(s // 2, []).append(start + size)
+        n = math.ceil(math.log2(size))
+        
+        if any(self.free_list[n:]):
+            self.allocate(process_id, size)
+        else:
+            print(f"Process {process_id} could not be allocated")
+
+    def allocate(self, process_id, size):
+        n = math.ceil(math.log2(size))
+
+        for i in range(n, self.size):
+            if self.free_list[i]:
+                start, end = self.free_list[i].pop(0)
+                while i > n:
+                    i -= 1
+                    mid = (start + end) // 2
+                    self.free_list[i].append((mid + 1, end))
+                    end = mid
                 self.processes[process_id] = (start, size)
                 print(f"Process {process_id} allocated {size} bytes from {start} to {start + size - 1}")
                 return
+
         print(f"Process {process_id} could not be allocated")
 
     def remove_process(self, process_id):
@@ -27,12 +46,18 @@ class BuddySystem:
             start, size = self.processes.pop(process_id)
             while size <= self.total_memory:
                 buddy = start ^ size
-                if buddy in self.free_blocks.get(size, []):
-                    self.free_blocks[size].remove(buddy)
-                    start = min(start, buddy)
-                    size *= 2
-                else:
-                    self.free_blocks.setdefault(size, []).append(start)
+                buddy_found = False
+
+                for i, (free_start, free_end) in enumerate(self.free_list[math.ceil(math.log2(size))]):
+                    if free_start == buddy:
+                        self.free_list[math.ceil(math.log2(size))].pop(i)
+                        start = min(start, buddy)
+                        size *= 2
+                        buddy_found = True
+                        break
+
+                if not buddy_found:
+                    self.free_list[math.ceil(math.log2(size))].append((start, start + size - 1))
                     break
             print(f"Process {process_id} removed from memory block {start} to {start + size - 1}")
         else:
@@ -44,8 +69,9 @@ class BuddySystem:
         for start, size in self.processes.values():
             for i in range(start, start + size):
                 allocated_memory[i] = 1
-        print_memory_layout(allocated_memory, 10)  # Assuming block size of 10 for display
+        print_memory_layout(allocated_memory, 10)
         print_allocation_table(self.processes)
-        
+
     def p_exists(self, process_id):
         return exists(self.processes, process_id)
+
